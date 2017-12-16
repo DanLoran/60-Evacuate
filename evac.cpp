d8b9ec67b4f5a66f14d238cd0c418a3dbd7c1555
Daniel Loran <dcloran@ucdavis.edu>
2017-12-15 16:30:52 -0800

    Changed to breadth first search queue when writing evac Routes. Calculate the depth of interior evac cities. Check for lack of population in evac cities in order to end instead of checking for null cities. Now track the minimum depth of cities with evacuees.

--- evac.cpp
+++ evac.cpp
@@ -7,66 +7,178 @@
 
 using namespace std;
 
+MyCity MyCity::operator = (City const &oldCity)
+{
+  ID = oldCity.ID;
+  x = oldCity.x;
+  y = oldCity.y;
+  roadCount = oldCity.roadCount;
+  population = oldCity.population;
+  evacuees = oldCity.evacuees;
+
+  roads = new Road[roadCount];
+  for(int j = 0; j < roadCount; j++) //deep copy all roads
+  {
+    roads[j] = oldCity.roads[j];
+  }
+  return *this;
+}
+
 Evac::Evac(City *citie, int numCitie, int numRoad) : numCities(numCitie), numRoadIDs(numRoad * 2), time(1)
 {
   //change the name to cityAr later
-  cityList = new City[numCities];
-  roadsUsed = new bool[numRoadIDs]();
-  cityDepth = new int[numCities];
-  //finalSprint = false;
+  cityList = new MyCity[numCities];
+  cityVisited = new bool[numCities](); //This will be used during bfsEvac.
 
   for(int i = 0; i < numCities; i++)
   {
-    cityList[i] = citie[i]; //sort by ID
-    cityList[i].roads = new Road[cityList[i].roadCount];
-    cityDepth[i] = -9001; //it's under 9000!!!!
-    for(int j = 0; j < citie[i].roadCount; j++) //deep copy all roads
-    {
-      cityList[i].roads[j] = citie[i].roads[j];
-    }
+    cityList[i] = citie[i]; //copy city into MyCity class
   }
 } // Evac()
 
 
-void Evac::evacuate(int *evacIDs, int numEvacs, EvacRoute *evacRoutes,
+void Evac::evacuate(int *evacIDs, int numEvac, EvacRoute *evacRoutes,
     int &routeCount)
 {
+  numEvacs = numEvac;
   routeCount = 0;
-  evacCities = new City*[numEvacs];
+  evacCities = new MyCity*[numEvacs];
 
   for(int i = 0; i < numEvacs; i++)
   {
     evacCities[i] = &cityList[evacIDs[i]]; //create array of evac cities
+    evacCities[i]->isEvacCity = true;
   }
-  calcDepth(numEvacs);
-  //cout << "made it to the start of the while loop" << endl;
-  while(checkEvacs()) //check if we still have evac cities with people in them
-  {
-    for(int indx = 0; indx < numEvacs; indx++)
-    { //we are looping through evacCities
-      City* curECity = evacCities[indx]; //get current evacCity
 
+  calcDepth(numEvacs);
+
+  while(!isEvacComplete()) //check if we still have evac cities with people in them
+  {
+    /*for(int indx = 0; indx < numEvacs; indx++)
+    { //we are looping through evacCities
+      MyCity* curECity = evacCities[indx]; //get current evacCity
+      //CHANGED: check for people, not NULL ptr
       if(curECity->population + curECity->evacuees > 0) //we still have people in this city
       {
         //pushes people out of all roads and generates evacRoutes
         vacateEvac(curECity, routeCount, evacRoutes);
+        //CHANGED: removed check for empty city and nulling ptrs
       }
       resetUsed(); //reset roads visited to false
-    }
+    }*/
+    bfsEvac(minDepth, routeCount, evacRoutes); // NOTE still writing
+    //updateMinDepth();//   NOTE should be done in isEvacComplete needs to be written. Updates minDepth if need be, should also update minDepthIndex.
     time++;
     //cout << "Time Incremented" << endl;
   }
 
 } // evacuate
 
-bool Evac::checkEvacs()
+void Evac::bfsEvac(int minDepth, int &routeCount, EvacRoute* evacRoutes)
 {
-  for(int i = 0; i < numEvacs; i++)
+  //Should be called exactly once per hour.
+  //First we enqueue all the cities with depth == minDepth
+  Queue<MyCity*> bfsQueue(numCities);
+  MyCity* curCity;
+  MyCity* adjCity;
+  Road curRoad;
+  int adjCityID;
+
+  for(int i = minDepthIndex; i < numEvacs; i++)
   {
+    if(evacCities[i]->depth == minDepth)
+    {
+      if(evacCities[i]->population + evacCities[i]->evacuees > 0)
+      { //we have people in this city
+        cityVisited[evacCities[i]->ID] = true; //mark each added city as visited.
+        bfsQueue.enqueue(&cityList[i]);
+      }
+    }
+    else
+    {
+      //If this triggers, there are no more cities that have minDepth.
+      break;
+    }
+  }
+  //Do bfs, writing evacRoute(s) as we go.
+  while(!bfsQueue.isEmpty())
+  {
+    curCity = bfsQueue.dequeue();
+    for(int i = 0; i < curCity->roadCount; i++)
+    {
+      adjCityID = curCity->roads[i].destinationCityID;
+      adjCity = &cityList[adjCityID];
+      if(validERoute(curCity, adjCity)) //check that adj has greater depth
+      {
+        //add adjCity to the queue if need be.
+        if(!cityVisited[adjCityID])
+        { //city has not been enqueued
+          bfsQueue.enqueue(adjCity);
+          cityVisited[adjCityID] = true; //now its been visited
+        }
+        //write the evacRoute
+        curRoad = curCity->roads[i];
+        int pplMoved;
+        EvacRoute eRoute;
+        eRoute.roadID = curRoad.ID;
+        eRoute.time = time;
+
+        //Use bool multiplication to make sure that we don't take population into account if we're not an evacCity.
+        pplMoved = min(curRoad.peoplePerHour, (curCity->evacuees + curCity->population * curCity->isEvacCity)); //Makes sure that we don't remove more people than the source city containsThiscodewrittenbyD4ni3lCLor4nand3mily/\/\H3ry
+        /*if(pplMoved < adjCity->population - adjCity->evacuees)
+        { //we have space in the city
+
+        }
+        else */ if ( adjCity->population != adjCity->evacuees)
+        { //we have a little bit of space
+          pplMoved = min(pplMoved, adjCity->population - adjCity->evacuees); ////find max people that can move to adjCity
+        }
+        else
+        {
+          continue; //don't make evacRoute
+        }
+        //we have people to put in our evacRoute
+        eRoute.numPeople = pplMoved;
+        adjCity->evacuees += pplMoved;
+
+        if(curCity->evacuees >= pplMoved)
+        {
+          curCity->evacuees -= pplMoved;
+        }
+        else
+        {
+          //First send off the evacuees, then the population.
+          pplMoved -= curCity->evacuees;
+          curCity->evacuees = 0;
+          curCity->population -= pplMoved;
+        }
+        evacRoutes[routeCount] = eRoute;
+        routeCount++;
+
+        if((adjCity->evacuees + adjCity->population) <= 0)
+        {
+          break; //Stop looking through roads if srcCity has no one left 0.
+        }
+      }
+    }
+  }
+  //Then reset cityVistied.
+}
+
+bool Evac::isEvacComplete() //CHANGED: new function checks for people in evacCities
+{
+  for(int i = minDepthIndex; i < numEvacs; i++)
+  {
+    if(evacCities[i]->depth != minDepth)
+    { //all the cities of the current min depth are empty
+      minDepthIndex = i; //this is the first element of this depth
+      minDepth = evacCities[i]->depth; //this is now the mindepth
+    }
     if(evacCities[i]->population + evacCities[i]->evacuees > 0)
     { //we find a city with people in it
       return false;
     }
+
   }
   return true;
 }
@@ -79,52 +191,116 @@ int Evac::min(int a, int b)
 
 void Evac::resetUsed()
 { //no cities have been visited
-  memset(roadsUsed, false, (sizeof(bool)*numRoadIDs));
+  memset(cityVisited, false, (sizeof(bool)*numCities));
   /*for(int i = 0; i < numRoadIDs; i++)
   {
     roadsUsed[i] = false;
   }*/
 }
 
-void Evac::calcDepth(int numEvacs)
+void Evac::findOuterRing(int numEvacs)
 {
-  Queue<City*> depthHolder(numCities);
-  City* curCity;
-  City* adjCity;
-  int adjCityID;
-  int nextDepth;
+
+  int numRoads;
+  int destCityID;
   for(int i = 0; i < numEvacs; i++)
   {
-    depthHolder.enqueue(evacCities[i]); //put evac cities in queue
-    cityDepth[evacCities[i]->ID] = 0; //set depth to zero
-  }
-
-  while(!depthHolder.isEmpty())
-  {
-    curCity = depthHolder.dequeue();
-    nextDepth = cityDepth[curCity->ID] + 1;
-    for(int i = 0; i < curCity->roadCount; i++) //enqueue all unvisited adjacent cities and mark their depth.
+    numRoads = evacCities[i]->roadCount;
+    for(int j = 0; i < numRoads; j++)
     {
-      adjCityID = curCity->roads[i].destinationCityID;
-      adjCity = &cityList[adjCityID];
-      if(cityDepth[adjCityID] == -1) //This city has not yet been assigned a depth.
+      destCityID = evacCities[i]->roads[j].destinationCityID;
+      if(!cityList[destCityID].isEvacCity)
       {
-        cityDepth[adjCityID] = nextDepth;
-        depthHolder.enqueue(adjCity);
+        evacCities[i]->depth = 0;
+        break;
       }
     }
 
   }
+}
+
+int comprar(const void* p1, const void* p2)
+{
+  if((**(MyCity**)p1).depth < (**(MyCity**)p2).depth) return -1;
+  if((**(MyCity**)p1).depth == (**(MyCity**)p2).depth) return 0;
+  if((**(MyCity**)p1).depth > (**(MyCity**)p2).depth) return 1;
+}
+
+void Evac::calcDepth(int numEvacs)
+{
+  Queue<MyCity*> depthHolder(numCities);
+  MyCity* curCity;
+  MyCity* adjCity;
+  int adjCityID;
+  int nextDepth;
+
+  findOuterRing(numEvacs); //relies on isEvacCity being properly initialized.
+
+  //First, we calculate the depth of all inner evac cities.
+  for(int i = 0; i < numEvacs; i++)
+  {
+    if(evacCities[i]->depth == 0)//Put all of the outer ring in a queue
+    {
+      depthHolder.enqueue(evacCities[i]);
+    }
+  }
+
+  while(!depthHolder.isEmpty())
+  {
+    curCity = depthHolder.dequeue(); //Capture the city at the top of the stack
+    nextDepth = curCity->depth - 1; //Depth counts down when moving inward from the ring.
+    for(int i = 0; i < curCity->roadCount; i++) //enqueue all unvisited adjacent cities and mark their depth.
+    {
+      adjCityID = curCity->roads[i].destinationCityID;
+      adjCity = &cityList[adjCityID];
+      if(adjCity->depth == -9001 && adjCity->isEvacCity) //Check that adjCity has not yet been assigned a depth and is an evacCity.
+      {
+        adjCity->depth = nextDepth;
+        depthHolder.enqueue(adjCity);
+      }
+    }
+  }
+
+  qsort(evacCities, numEvacs, sizeof(MyCity*), comprar); //This should sort all the pointers in evacCities by depth.
+  minDepth = evacCities[0]->depth; //Initialize minDepth to lowest possible depth.
+  minDepthIndex = 0; //first element at mindepth is at pos 0 in evac array
+
+  //Now we calculate depth for the non-evac cities. Queue is now empty because while loop finished.
+
+  for(int i = 0; i < numEvacs; i++)
+  {
+    if(evacCities[i]->depth == 0)
+    {
+      depthHolder.enqueue(evacCities[i]); //Once again, we have to enqueue the outer ring.
+    }
+  }
+
+  while(!depthHolder.isEmpty())
+  {
+    curCity = depthHolder.dequeue(); //Capture the city at the top of the stack
+    nextDepth = curCity->depth + 1; //Depth counts up when moving inward from the ring.
+    for(int i = 0; i < curCity->roadCount; i++) //enqueue all unvisited adjacent cities and mark their depth.
+    {
+      adjCityID = curCity->roads[i].destinationCityID;
+      adjCity = &cityList[adjCityID];
+      if(adjCity->depth == -9001) //Check that adjCity has not yet been assigned a depth, every city without a depth is not an evacCity.
+      {
+        adjCity->depth = nextDepth;
+        depthHolder.enqueue(adjCity);
+      }
+    }
+  }
 
 }
-bool Evac::vacateEvac(City *srcCity, int &routeCount, EvacRoute *evacRoutes)
+/*
+bool Evac::vacateEvac(MyCity *srcCity, int &routeCount, EvacRoute *evacRoutes)
 {
   bool isInternal = checkInternal(srcCity);
 
   for(int i = 0; i < srcCity->roadCount; i++)
   {
     Road curRoad = srcCity->roads[i];
-    City* dstCity = &cityList[curRoad.destinationCityID];
+    MyCity* dstCity = &cityList[curRoad.destinationCityID];
 
     if(validERoute(isInternal, srcCity, dstCity, curRoad))
     {
@@ -180,7 +356,7 @@ bool Evac::vacateEvac(City *srcCity, int &routeCount, EvacRoute *evacRoutes)
   return false;
 }
 
-bool Evac::checkInternal(City* internal)
+bool Evac::checkInternal(MyCity* internal)
 {
   for (int i = 0; i < internal->roadCount; i++)
   {
@@ -193,12 +369,12 @@ bool Evac::checkInternal(City* internal)
 }
 //Should accept bool isEvacCity, set to true if srcCity is an evacCity.
 //Changed: now accepts a bool isEvacCity
-void Evac::vacateCity(City *srcCity, int &routeCount, EvacRoute *evacRoutes)
+void Evac::vacateCity(MyCity *srcCity, int &routeCount, EvacRoute *evacRoutes)
 {
   for(int i = 0; i < srcCity->roadCount; i++)
   {
     Road curRoad = srcCity->roads[i];
-    City* dstCity = &cityList[curRoad.destinationCityID];
+    MyCity* dstCity = &cityList[curRoad.destinationCityID];
     //TODO: add depth check
     if(validERoute(false, srcCity, dstCity, curRoad)) //make sure we haven't used this road already
     {
@@ -235,10 +411,10 @@ void Evac::vacateCity(City *srcCity, int &routeCount, EvacRoute *evacRoutes)
     }
   }
 }
-
-bool Evac::validERoute(bool isInternal, City* srcCity, City* dstCity, Road curRoad)
+*/
+bool Evac::validERoute(MyCity* srcCity, MyCity* dstCity)
 {
-  if(roadsUsed[curRoad.ID])
+  /*if(roadsUsed[curRoad.ID])
   {
     return false;
   }
@@ -247,6 +423,10 @@ bool Evac::validERoute(bool isInternal, City* srcCity, City* dstCity, Road curRo
     return false;
   }
   else if (dstCity->population <= 0)
+  {
+    return false;
+  }*/
+  if(srcCity->depth >= dstCity->depth)
   {
     return false;
   }
--- evac.h
+++ evac.h
@@ -5,49 +5,58 @@
 #include "EvacRunner.h"
 #include "LinkedList.h"
 
+class MyCity
+{
+public:
+  int ID;
+  int x;
+  int y;
+  int population;
+  int evacuees;
+  Road *roads;
+  int roadCount;
+  int depth; //added
+  int isEvacCity; //added
+  MyCity() : evacuees(0), roads(NULL), roadCount(0), depth(-9001), isEvacCity(false){}
+  //MyCity(City oldCity);
+  MyCity operator = (City const &oldCity);
+  ~MyCity(){delete [] roads;}
+};
+
 class Evac
 {
 public:
   Evac(City *citie, int numCitie, int numRoad);
-  void evacuate(int *evacIDs, int numEvacs, EvacRoute *evacRoutes,
+  void evacuate(int *evacIDs, int numEvac, EvacRoute *evacRoutes,
     int &routeCount); // student sets evacRoutes and routeCount
 
 private:
-  City* cityList;
-  bool* roadsUsed;
-  City** evacCities;
-  int* cityDepth;
+  MyCity* cityList;
+  bool* cityVisited;
+  MyCity** evacCities;
   int numCities;
   int numRoadIDs;
   int time;
-  int remainingEvacs;
+  int numEvacs;
+  int minDepth;
+  int minDepthIndex; //is index of first city of minDepth depth.
 
   bool vacateEvac(City *srcCity, int &routeCount, EvacRoute *evacRoutes); //returns if srcCity empties
   void vacateCity(City* srcCity, int &routeCount, EvacRoute *evacRoutes);
-  bool Evac::checkEvacs();
+  bool isEvacComplete(); //CHANGED: added checkEvacs
   int min(int a, int b);
   void resetUsed();
   bool checkInternal(City* internal);
   void calcDepth(int numEvacs);
-  bool validERoute(bool isInternal, City* srcCity, City* dstCity, Road curRoad);
+  bool validERoute(MyCity* srcCity, MyCity* dstCity);
+
+
+  void findOuterRing(int numEvacs);
+  void bfsEvac(int minDepth, int &routeCount, EvacRoute* EvacRoutes);
   //bool finalSprint;
 
 }; // class Evac
 
-
-//int compare (const void * a, const void * b);
-
-/*class UWTable
-{
-private:
-  int tableSize;
-  bool* known;
-  int* depth;
-  int* roadCapacity;
-  int* prevCityID;
-public:
-  UWTable(int size);
-  fill();
-}*/
+int comprar(const void* p1, const void* p2);
 
 #endif
